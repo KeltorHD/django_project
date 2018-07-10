ec4ce060aa697dbd5559057c4e1224ff2b4b0009
Keltor <keltorplaylife@gmail.com>
2018-07-10 20:25:14 +0300

    Минорное обновление

---
+++ app/static/js/script.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+$( ".hidden" ).hide();
+$( ".menu" ).hide();
+
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
