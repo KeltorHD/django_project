c0b6f3e7d32df90e347424bb4723691085251930
Keltor <keltorplaylife@gmail.com>
2018-08-03 19:24:05 +0300

    Багфикс js

--- app/static/js/table.js
+++ app/static/js/table.js
@@ -1,5 +1,5 @@
 function settingUpTable() {
-    $('#table').dataTable({
+    $('#table').DataTable( {
         "oLanguage": {
             "sLengthMenu": "Показать _MENU_ записей на странице",
             "sZeroRecords": "Извините - ничего не найдено",
@@ -14,11 +14,9 @@ function settingUpTable() {
         }
     });
 }
- 
 $(document).ready(function () {
-    settingUpTable();
-});
-
+    settingUpTable();        
+})
 var tableToExcel = (function() {
   var uri = 'data:application/vnd.ms-excel;base64,'
     , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
